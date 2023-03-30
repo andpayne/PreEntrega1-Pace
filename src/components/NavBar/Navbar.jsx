05b622f409ee107f88050a093b197fae810b98db
Andres <andpace89@gmail.com>
2023-03-30 14:24:12 -0300

    agregue css

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,9 +1,30 @@
-//import React from 'react';
+import { useRef } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 export const NavBar = () => {
+    const navRef = useRef();
+
+    const showNavbar = () => {
+        navRef.current.classList.toogle('responsive_nav')
+    }
+    
     return (
         <div>
-            <h1>NavBar</h1>     
+            <header>
+                <h3>Logo</h3>
+                <nav ref={navRef}>
+                    <a href="#">Accesorios</a>
+                    <a href="#">Celulares</a>
+                    <a href="#">Computadoras</a>
+                    <a href="#">Televisores</a>
+                    <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+                        <FaTimes/>
+                    </button>
+                </nav>
+                <button className="nav-btn" onClick={showNavbar}>
+                    <FaBars/> 
+                </button>
+            </header>    
         </div>
     );
 }
